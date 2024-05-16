f2dba5100af6ecbf6b09b28dcaa3f9a7fb2dea53
Paula-Stefany <paula.sfa@outlook.com>
2024-05-16 14:39:43 -0300

    form de login e html de login

--- floriCultura/forms/auth/CadasterForm.py
+++ floriCultura/forms/auth/CadasterForm.py
@@ -3,8 +3,8 @@ from django import forms
 
 class CadasterForm(forms.Form):
     username = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'input-box', 'placeholder': 'Nome *'}))
-    email = forms.CharField(max_length=50, required=True, widget=forms.TextInput(attrs={'class': 'input-box', 'placeholder': 'Email *'}))
+    email = forms.EmailField(max_length=50, required=True, widget=forms.EmailFieldInput(attrs={'class': 'input-box', 'placeholder': 'Email *'}))
     cpf = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={
     'class': 'input-box', 'placeholder': 'CPF *'}))
-    senha1 = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'input-box', 'placeholder': 'Senha *'}))
-    senha2 = forms.CharField(max_length=20, required=True, widget=forms.TextInput(attrs={'class': 'input-box', 'placeholder': 'Confirme a Senha *'}))
+    senha1 = forms.CharField(max_length=20, required=True, widget=forms.PasswordInput(attrs={'class': 'input-box', 'placeholder': 'Senha *'}))
+    senha2 = forms.CharField(max_length=20, required=True, widget=forms.PasswordInput(attrs={'class': 'input-box', 'placeholder': 'Confirme a Senha *'}))
