f3a3e71b7516e203bbe04e25cace457268d4ccad
Paula-Stefany <paula.sfa@outlook.com>
2024-06-18 18:10:18 -0300

    aplicação csrf_token e ajuste na tela de recuperação de senha

--- floriCultura/forms/auth/RecoveryForm.py
+++ floriCultura/forms/auth/RecoveryForm.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class RecoveryForm(forms.Form):
-    email = forms.EmailField(required=True, label='Email para Recuperação de Senha', widget=forms.EmailInput(attrs={'class': 'input-box', 'placeholder': 'Email *', 'id': 'email'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'input-box', 'placeholder': 'Email *', 'id': 'email'}))
